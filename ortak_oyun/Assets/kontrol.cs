548ffdf9198704c5f5dc8aa8617df13a9b533c64
denzi <denizceylan2002@gmail.com>
2022-07-01 23:37:07 +0300

    bi şey ekledim

---
+++ ortak_oyun/Assets/kontrol.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kontrol : MonoBehaviour
+{
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+                rb.AddForce(Vector2.up * 10 * Time.deltaTime);
+    }
+    
+}
